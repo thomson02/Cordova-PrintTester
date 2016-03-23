ae92b3d03a53d73f9e91d4b051ee6d6325d958e2
thomson02 <andrew.thomson@spartan-solutions.com>
2016-03-23 10:08:46 +0000

    use bower_components instead of moving everything lib dir. Use gulp task to move individual files that are required.

---
+++ PrintTester/gulpfile.js
@@ -0,0 +1,11 @@
+﻿/// <binding BeforeBuild='BEFORE_BUILD' />
+var gulp = require("gulp");
+
+
+gulp.task('moveJQueryToLib', function() {
+    return gulp.src('./bower_components/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest('./www/lib/'));
+});
+
+
+gulp.task('BEFORE_BUILD', ['moveJQueryToLib']);
