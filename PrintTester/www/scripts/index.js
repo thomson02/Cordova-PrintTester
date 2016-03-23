4375d69d7f63a7f1267b9bf8526d43b95438eb5b
thomson02 <andrew.thomson@spartan-solutions.com>
2016-03-23 16:59:43 +0000

    CPCL format label

--- PrintTester/www/scripts/index.js
+++ PrintTester/www/scripts/index.js
@@ -7,6 +7,110 @@
 
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
     
+    // First, checks if it isn't implemented yet.
+    if (!String.prototype.format) {
+        String.prototype.format = function () {
+            var args = arguments;
+            return this.replace(/{(\d+)}/g, function (match, number) {
+                return typeof args[number] != 'undefined'
+                  ? args[number]
+                  : match
+                ;
+            });
+        };
+    }
+
+    function constructPrintString(printItems) {
+
+        var zebraCpclCommandPrefix = '! 0 200 200 210 1'; //'! 0 200 200 550 1';
+        var cursorYPosition = 0;
+
+        var config = {
+            textLineHeight: 40,
+            barcodeHeight: 40,
+            barcodeEncoding: "128",
+            font: 0,
+            fontSize: 3,
+            maxCharsPerLine: 36,
+            maxCharsPerLabel: 9
+        };
+
+        var dataString = [];
+        dataString.push(zebraCpclCommandPrefix);
+
+        for (var i = 0; i < printItems.length; i++) {
+            if (printItems[i].text) {
+                // TEXT {font} {size} {x} {y} {data}
+                dataString.push('T {0} {1} {2} {3} {4}'.format(
+                        config.font,
+                        config.fontSize,
+                        0,
+                        cursorYPosition,
+                        printItems[i].text.substring(0, config.maxCharsPerLine)));
+
+                cursorYPosition += config.textLineHeight;
+            }
+            else if (printItems[i].barcode) {
+
+                if (printItems[i].showText) {
+                    // BARCODE-TEXT {font} {size} {offset}
+                    dataString.push("BT {0} {1} {2}".format(
+                        config.font,
+                        config.fontSize,
+                        5));
+                }
+
+                // BARCODE {code} {width} {ratio} {height} {x} {y} {data}
+                dataString.push("B {0} {1} {2} {3} {4} {5} {6}".format(
+                        config.barcodeEncoding,
+                        1,
+                        1,
+                        config.barcodeHeight,
+                        0,
+                        cursorYPosition,
+                        printItems[i].barcode
+                    ));
+
+                cursorYPosition += config.barcodeHeight + 10 + (printItems[i].showText ? config.textLineHeight : 0);
+
+                if (printItems[i].showText) {
+                    dataString.push("BT OFF");
+                }
+            }
+            else if (printItems[i].line) {
+
+                // TEXT {font} {size} {x} {y} {data}
+                dataString.push("T {0} {1} {2} {3} {4}".format(
+                        config.font,
+                        config.fontSize,
+                        0,
+                        cursorYPosition,
+                        Array(config.maxCharsPerLine).join('-')
+                    ));
+
+                cursorYPosition += config.textLineHeight;
+            }
+        }
+
+        dataString.push("FORM");    // form feed after printing
+        dataString.push("PRINT");   // terminate and print the file
+
+        return dataString.join('\r\n');
+    };
+
+    function fakeLabel() {
+        var printItems = [
+            { text: "Supplier:" },
+            { text: "Company Name" },
+            { text: "Received: 23 Mar 2016" },
+            { text: "GRN#:" },
+            { barcode: "12345", showText: true },
+            { line: true }
+        ];
+
+        return constructPrintString(printItems);
+    };
+
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
@@ -17,7 +121,7 @@
         element.innerHTML = 'Device Ready';
         element.className += ' ready';
 
-        var associatedPrinterMac = null;
+        var associatedPrinterMac = "AC:3F:A4:0D:F2:1B"; //null;
 
         // Hook up TC55 linear barcode scanner
         if (window.datawedge) {
@@ -54,7 +158,7 @@
                     })
                     .then(function() {
                         var p = $.Deferred();
-                        window.cordova.plugins.zbtprinter.print(associatedPrinterMac, "^XA^FO10,10^AFN,26,13^FDHello, World!^FS^XZ", p.resolve, p.reject);
+                        window.cordova.plugins.zbtprinter.print(associatedPrinterMac, fakeLabel(), p.resolve, p.reject);
                         return p;
                     })
                     .fail(function(err) {
